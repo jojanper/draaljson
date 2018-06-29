1c5acd9f58c06d80fce9bf4250d7be889779b272
Juha Ojanperä <juha.ojanpera@gmail.com>
2018-06-29 12:25:29 +0300

    More unit tests for JsonWriter.

--- src/tests/json.spec.js
+++ src/tests/json.spec.js
@@ -1,10 +1,19 @@
 const { JsonWriter } = require('../json');
 const { log } = require('../utils');
 
+
 describe('JsonWriter', () => {
     beforeEach(() => {
         spyOn(log, 'logError');
     });
+
+    function runTest(manifest, env, done) {
+        JsonWriter.create(manifest, env).then((response) => {
+            expect(response).toBeUndefined();
+            expect(log.logError).toHaveBeenCalledTimes(1);
+            done();
+        });
+    }
 
     it('write fails due to invalid manifest path', (done) => {
         const manifest = {
@@ -15,10 +24,34 @@ describe('JsonWriter', () => {
             }
         };
 
-        JsonWriter.create(manifest, 'dev').then((response) => {
-            expect(response).toBeUndefined();
-            expect(log.logError).toHaveBeenCalledTimes(1);
-            done();
-        });
+        runTest(manifest, 'dev', done);
+    });
+
+    it('write fails due to invalid input schema path', (done) => {
+        const manifest = {
+            environments: {
+                dev: {
+                    path: 'test/fixtures/environments/dev/manifest.json',
+                    inputSchema: 'test/fixtures/schema/input-schema-invalid.json',
+                    outputSchema: 'test/fixtures/schema/output-schema.json'
+                }
+            }
+        };
+
+        runTest(manifest, 'dev', done);
+    });
+
+    it('write fails due to invalid output schema path', (done) => {
+        const manifest = {
+            environments: {
+                dev: {
+                    path: 'test/fixtures/environments/dev/manifest.json',
+                    inputSchema: 'test/fixtures/schema/input-schema.json',
+                    outputSchema: 'test/fixtures/schema/output-schema-invalid.json'
+                }
+            }
+        };
+
+        runTest(manifest, 'dev', done);
     });
 });
